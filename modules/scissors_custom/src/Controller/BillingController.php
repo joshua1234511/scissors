9b27079dcf51ccfbd6d50e59410b14898d1cbc8b
Joshua Fernandes <joshua@sjinnovation.com>
2019-03-16 22:16:22 +0530

    Added billing code

--- modules/scissors_custom/src/Controller/BillingController.php
+++ modules/scissors_custom/src/Controller/BillingController.php
@@ -23,23 +23,164 @@ class BillingController extends ControllerBase
   public function viewBilling(AccountInterface $user)
   {
     \Drupal::service('page_cache_kill_switch')->trigger();
-    $CI = $this->billingAccess();
-    $o = "";
-    $element = array(
-      '#markup' => $o,
-    );
-    return $element;
+    $user = \Drupal::currentUser();
+    $user_email = $user->getEmail();
+    $connection = \Drupal::database();
+    $query = $connection->select('ospos_people', 'p');
+    $query->condition('p.email', $user_email);
+    $query->fields('p',['person_id']);
+    $query->leftJoin('ospos_customers', 'c', 'p.person_id = c.person_id');
+    $query->condition('c.person_id', '', '<>');
+    $query->fields('c'['person_id']);
+    $query->range(0,1);
+    $result = $query->execute()->fetchField();
+    if(!empty($result)){
+      // Sales
+      $saleRecords = array();
+      $person_id = $result;
+      $query = $connection->select('ospos_sales', 's');
+      $query->condition('s.customer_id', $person_id);
+      $query->condition('s.sale_status', 0);
+      $query->fields('s',['sale_time','sale_id']);
+      $query->orderBy('s.sale_time', 'DESC');
+      $saleResult = $query->execute();
+      if(!empty($saleResult)){
+        foreach ($saleResult as $sale) {
+          $saleRecords[$sale->sale_id]['sales'] = (array)$sale;
+          $saleRecords[$sale->sale_id]['sales']['total'] = 0;
+          $saleRecords[$sale->sale_id]['sales']['sub_total'] = 0;
+          $saleRecords[$sale->sale_id]['sales']['quantity'] = 0;
+          $saleRecords[$sale->sale_id]['sales']['payment_type'] = array();
+          // Sales Item
+          $saleItems = array();
+          $query = $connection->select('ospos_sales_items', 'i');
+          $query->condition('i.sale_id', $sale->sale_id);
+          $query->fields('i',['item_id','quantity_purchased','item_cost_price','discount_percent']);
+          $query->leftJoin('ospos_items', 'a', 'i.item_id = a.item_id');
+          $query->fields('a',['name']);
+          $saleItemsResult = $query->execute();
+          if(!empty($saleItemsResult)){
+            foreach ($saleItemsResult as $saleItem) {
+              $saleItems[$saleItem->item_id] = (array)$saleItem;
+              $saleItems[$saleItem->item_id]['total'] = $saleItem->item_cost_price * $saleItem->quantity_purchased;
+              $saleItems[$saleItem->item_id]['total'] = $saleItems[$saleItem->item_id]['total'] - (($saleItem->discount_percent / 100) * $saleItems[$saleItem->item_id]['total']);
+              $saleItems[$saleItem->item_id]['sub_total'] = $saleItem->item_cost_price * $saleItem->quantity_purchased;
+              $saleRecords[$sale->sale_id]['sales']['sub_total'] += $saleItems[$saleItem->item_id]['sub_total'];
+              $saleRecords[$sale->sale_id]['sales']['total'] += $saleItems[$saleItem->item_id]['total'];
+              $saleRecords[$sale->sale_id]['sales']['quantity'] += $saleItem->quantity_purchased;
+            }
+            $saleRecords[$sale->sale_id]['items'] = (array)$saleItems;
+          }
+          // Sales Payment
+          $salePayments = array();
+          $query = $connection->select('ospos_sales_payments', 'pay');
+          $query->condition('pay.sale_id', $sale->sale_id);
+          $query->fields('pay',['payment_amount','payment_type']);
+          $salePaymentsResult = $query->execute();
+          if(!empty($salePaymentsResult)){
+            foreach ($salePaymentsResult as $salePayment) {
+              $salePayments[] = (array)$salePayment;
+              $saleRecords[$sale->sale_id]['sales']['payment_type'][] = $salePayment->payment_type.' '.$salePayment->payment_amount;
+            }
+            $saleRecords[$sale->sale_id]['payments'] = (array)$salePayments;
+          }
+          // Sales Reward
+          $saleRewards = array();
+          $query = $connection->select('ospos_sales_reward_points', 'r');
+          $query->condition('r.sale_id', $sale->sale_id);
+          $query->fields('r',['earned','used']);
+          $saleRewardsResult = $query->execute();
+          if(!empty($saleRewardsResult)){
+            foreach ($saleRewardsResult as $saleReward) {
+              $saleRewards[] = (array)$saleReward;
+            }
+            $saleRecords[$sale->sale_id]['rewards'] = (array)$saleRewards;
+          }
+         }
+      }
+    }
+    $header = [
+      ['data' => $this->t('Date')],
+      ['data' => $this->t('Quantity')],
+      ['data' => $this->t('Subtotal')],
+      ['data' => $this->t('Total')],
+      ['data' => $this->t('Payment Type')],
+    ];
+    $headerProducts = [
+      ['data' => $this->t('Name')],
+      ['data' => $this->t('Quantity')],
+      ['data' => $this->t('Subtotal')],
+      ['data' => $this->t('Total')],
+      ['data' => $this->t('Discount')],
+    ];
+    $headerRewards = [
+      ['data' => $this->t('Points Used')],
+      ['data' => $this->t('Points Earned')],
+    ];
+    $limit = 20;
+    $saleRecords = $this->pagerArraySplice($saleRecords, $limit);
+    if(!empty($saleRecords)){
+      foreach ($saleRecords as $key => $value) {
+        $build['config_table'][] = [
+          '#markup' => '<h3>Trans. ID: '.$value['sales']['sale_id'].'</h3>',
+        ];
+        $saleDetails = array();
+        $saleDetails[$key][] = $value['sales']['sale_time'];
+        $saleDetails[$key][] = $value['sales']['quantity'];
+        $saleDetails[$key][] = $value['sales']['sub_total'];
+        $saleDetails[$key][] = $value['sales']['total'];
+        $saleDetails[$key][] = implode(', ', $value['sales']['payment_type']);
+        $build['config_table'][] = [
+          '#theme' => 'table',
+          '#header' => $header,
+          '#rows' => $saleDetails,
+          '#empty' => t('Sorry, there is no results to show'),
+        ];
+        $rowsProducts = array();
+        foreach ($value['items'] as $key => $item) {
+          $rowsProducts[$key][] = $item['name'];
+          $rowsProducts[$key][] = $item['quantity_purchased'];
+          $rowsProducts[$key][] = $item['sub_total'];
+          $rowsProducts[$key][] = $item['total'];
+          $rowsProducts[$key][] = $item['discount_percent'].'%';
+        }
+        $build['config_table'][] = [
+          '#theme' => 'table',
+          '#header' => $headerProducts,
+          '#rows' => $rowsProducts,
+          '#empty' => t('Sorry, there is no results to show'),
+        ];
+        $rowsRewards = array();
+        foreach ($value['rewards'] as $key => $reward) {
+          $rowsRewards[$key][] = $reward['used'];
+          $rowsRewards[$key][] = $reward['earned'];
+        }
+        $build['config_table'][] = [
+          '#theme' => 'table',
+          '#header' => $headerRewards,
+          '#rows' => $rowsRewards,
+          '#empty' => t('Sorry, there is no results to show'),
+        ];
+      }
+    }
+    // Finally add the pager.
+    $build['pager'] = [
+      '#type' => 'pager',
+    ];
+    return $build;
   }
-
-  private function billingAccess($controller = 'login', $function = 'index'){
-    ob_start();
-    $_SERVER['QUERY_STRING'] = '';
-    $routing['controller'] = $controller;
-    $routing['function']  = $function;
-    define("REQUEST", "external");
-    require_once DRUPAL_ROOT.'\billing\public\external.php';
-    ob_end_clean();
-    $CI =& get_instance();
-    return $CI;
+  /**
+   * Array splice function.
+   */
+  public function pagerArraySplice($data, $limit = 10, $element = 0) {
+    global $pager_page_array, $pager_total, $pager_total_items;
+    $page = isset($_GET['page']) ? $_GET['page'] : '';
+    // Convert comma-separated $page to an array, used by other functions.
+    $pager_page_array = explode(',', $page);
+    // We calculate the total of pages as ceil(items / limit).
+    $pager_total_items[$element] = count($data);
+    $pager_total[$element] = ceil($pager_total_items[$element] / $limit);
+    $pager_page_array[$element] = max(0, min((int) $pager_page_array[$element], ((int) $pager_total[$element]) - 1));
+    return array_slice($data, $pager_page_array[$element] * $limit, $limit, TRUE);
   }
 }
---
+++ modules/scissors_custom/src/Plugin/Block/MyAccountDetailsBlock.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Drupal\scissors_custom\Plugin\Block;
+
+use Drupal\Core\Link;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Block\BlockBase;
+use Drupal\user\Entity\User;
+
+/**
+ * Class MyAccountDetailsBlock.
+ *
+ * @Block(
+ *   id = "my_account_details_block",
+ *   admin_label = @Translation("My Account Details Block"),
+ * )
+ */
+class MyAccountDetailsBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    \Drupal::service('page_cache_kill_switch')->trigger();
+    $o = '';
+    $user = \Drupal::currentUser();
+    $user_email = $user->getEmail();
+    $connection = \Drupal::database();
+    $query = $connection->select('ospos_people', 'p');
+    $query->condition('p.email', $user_email);
+    $query->fields('p');
+    $query->leftJoin('ospos_customers', 'c', 'p.person_id = c.person_id');
+    $query->condition('c.person_id', '', '<>');
+    $query->fields('c');
+    $query->range(0,1);
+    $result = $query->execute();
+    $customerDetails = array();
+    if(!empty($result)){
+      foreach ($result as $record) {
+        $record = (array) $record;
+        if(isset($record['first_name']) && !empty($record['first_name'])){
+          $customerDetails['first_name'] = $record['first_name'];
+        }
+        if(isset($record['last_name']) && !empty($record['last_name'])){
+          $customerDetails['last_name'] = $record['last_name'];
+        }
+        if(isset($record['phone_number']) && !empty($record['phone_number'])){
+          $customerDetails['phone_number'] = $record['phone_number'];
+        }
+        if(isset($record['email']) && !empty($record['email'])){
+          $customerDetails['email'] = $record['email'];
+        }
+        if(isset($record['address_1']) && !empty($record['address_1'])){
+          $customerDetails['address'][] = $record['address_1'];
+        }
+        if(isset($record['address_2']) && !empty($record['address_2'])){
+          $customerDetails['address'][] = $record['address_2'];
+        }
+        if(isset($record['city']) && !empty($record['city'])){
+          $customerDetails['address'][] = $record['city'];
+        }
+        if(isset($record['state']) && !empty($record['state'])){
+          $customerDetails['address'][] = $record['state'];
+        }
+        if(isset($record['zip']) && !empty($record['zip'])){
+          $customerDetails['address'][] = $record['zip'];
+        }
+        if(isset($record['country']) && !empty($record['country'])){
+          $customerDetails['address'][] = $record['country'];
+        }
+        if(isset($record['points']) && !empty($record['points'])){
+          $customerDetails['reward_points'] = $record['points'];
+        }
+      }
+    }
+    if(!empty($customerDetails)){
+      $o = '<div class="row">';
+      foreach ($customerDetails as $key => $value) {
+        if(is_array($value)){
+          $o .= '<p><span>'.ucwords(str_replace('_', ' ', $key)).':</span> ';
+          $o .= implode(', ', $value);
+          $o .= '</p>';
+        }else{
+          $o .= '<p><span>'.ucwords(str_replace('_', ' ', $key)).':</span> '.$value.'</p>';
+        }
+      }
+      $o .= '</div>';
+    }
+    return array(
+      '#markup' => $o,
+    );
+
+  }
+
+}
